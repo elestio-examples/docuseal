d72fc6bee15184f669b1e93605e91d7afd5e2ff6
Prem chavhan <pchavhan@logiquad.com>
2023-07-24 18:57:20 +0530

    updated fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,2 +1,8 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
+
+mkdir -p ./docuseal_data;
+chown -R 1001:1001 ./docuseal_data;
+
+mkdir -p ./pg_data;
+chown -R 1001:1001 ./pg_data;
